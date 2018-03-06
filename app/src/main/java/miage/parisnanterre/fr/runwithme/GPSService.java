87907f84c2567eed37c339f02783eb553ad3c432
Fabien <fabien.michl@gmail.com>
2018-03-06 09:34:15 +0100

    Update: location params

--- app/src/main/java/miage/parisnanterre/fr/runwithme/GPSService.java
+++ app/src/main/java/miage/parisnanterre/fr/runwithme/GPSService.java
@@ -16,8 +16,8 @@ public class GPSService extends Service
 {
     private static final String TAG = "GEOLOCALISATION";
     private LocationManager mLocationManager = null;
-    private static final int LOCATION_INTERVAL = 10000;
-    private static final float LOCATION_DISTANCE = 10f;
+    private static final int LOCATION_INTERVAL = 1000;
+    private static final float LOCATION_DISTANCE = 0;
 
     private class LocationListener implements android.location.LocationListener{
         Location mLastLocation;
