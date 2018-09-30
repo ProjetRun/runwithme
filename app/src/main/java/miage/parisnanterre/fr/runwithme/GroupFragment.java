c94bbee2b6371d61ca1c17942d12a8afbd0eda15
Fabien <fabien.michl@gmail.com>
2018-09-30 19:23:58 +0200

    Update : profil IHM & Add : WebView for running-news

---
+++ app/src/main/java/miage/parisnanterre/fr/runwithme/GroupFragment.java
@@ -0,0 +1,37 @@
+package miage.parisnanterre.fr.runwithme;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+
+public class GroupFragment extends Fragment {
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
+
+    private WebView webView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_group, parent, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+        webView = (WebView) view.findViewById(R.id.webView);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.loadUrl("http://www.jogging-plus.com/calendrier/courses-5-10-15-km/ile-de-france/");
+        //https://www.parisrunningtour.com/fr/competitions/
+    }
+
+}
--- app/src/main/java/miage/parisnanterre/fr/runwithme/ProfilFragment.java
+++ app/src/main/java/miage/parisnanterre/fr/runwithme/ProfilFragment.java
@@ -1,10 +1,13 @@
 package miage.parisnanterre.fr.runwithme;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 public class ProfilFragment extends Fragment {
@@ -16,11 +19,39 @@ public class ProfilFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_profil, parent, false);
     }
 
+    private TextView txtProgress;
+    private ProgressBar progressBar;
+    private int pStatus = 0;
+    private Handler handler = new Handler();
     // This event is triggered soon after onCreateView().
     // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Setup any handles to view objects here
         // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+
+        txtProgress = (TextView) view.findViewById(R.id.txtProgress);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (pStatus <= 95) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(pStatus);
+                            txtProgress.setText(pStatus + " %");
+                        }
+                    });
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    pStatus++;
+                }
+            }
+        }).start();
     }
 }
