446357fd0d5ecdca7c1b4afa3a229f0f17c5952e
Fabien <fabien.michl@gmail.com>
2018-10-01 18:47:07 +0200

    Add : chart, icon and webview

--- app/src/main/java/miage/parisnanterre/fr/runwithme/ProfilFragment.java
+++ app/src/main/java/miage/parisnanterre/fr/runwithme/ProfilFragment.java
@@ -1,13 +1,24 @@
 package miage.parisnanterre.fr.runwithme;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
 
 
 public class ProfilFragment extends Fragment {
@@ -23,6 +34,7 @@ public class ProfilFragment extends Fragment {
     private ProgressBar progressBar;
     private int pStatus = 0;
     private Handler handler = new Handler();
+
     // This event is triggered soon after onCreateView().
     // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
     @Override
@@ -53,5 +65,40 @@ public class ProfilFragment extends Fragment {
                 }
             }
         }).start();
+
+
+        LineChartView chart = (LineChartView) view.findViewById(R.id.chart);
+
+        List<PointValue> values = new ArrayList<PointValue>();
+        values.add(new PointValue(0, 2));
+        values.add(new PointValue(1, 4));
+        values.add(new PointValue(2, 3));
+        values.add(new PointValue(3, 4));
+
+        //In most cased you can call data model methods in builder-pattern-like manner.
+        Line line = new Line(values).setColor(Color.BLUE).setCubic(true);
+        List<Line> lines = new ArrayList<Line>();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+
+        chart.setLineChartData(data);
+
+        Button b = (Button) view.findViewById(R.id.buttonLaunchRunningStat);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                launchRunningStatActivity(v);
+            }
+        });
+
+
+    }
+
+    public void launchRunningStatActivity(View v){
+        Intent intent = new Intent(getActivity(), RunningStatisticsActivity.class);
+        startActivity(intent);
     }
 }
