7f52b37afe820d9931184c9821718191ca3dba65
Fabien <fabien.michl@gmail.com>
2018-10-22 23:08:37 +0200

    Update : workout fragement

--- app/src/main/java/miage/parisnanterre/fr/runwithme/BeforeWorkoutFragment.java
+++ app/src/main/java/miage/parisnanterre/fr/runwithme/BeforeWorkoutFragment.java
@@ -2,14 +2,18 @@ package miage.parisnanterre.fr.runwithme;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.truizlop.fabreveallayout.FABRevealLayout;
+import com.truizlop.fabreveallayout.OnRevealChangeListener;
 
 public class BeforeWorkoutFragment extends Fragment {
     // The onCreateView method is called when Fragment should create its View object hierarchy,
@@ -25,15 +29,22 @@ public class BeforeWorkoutFragment extends Fragment {
     private ProgressBar progressBar;
     private int progressStatus = 0;
     private TextView textView;
+    FloatingActionButton fab;
+    private ImageButton pause;
+    private ImageButton skip;
     private Handler handler = new Handler();
+    Thread th;
     // This event is triggered soon after onCreateView().
     // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Setup any handles to view objects here
         // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
-        img = (ImageView) view.findViewById(R.id.imageView17);
+        FABRevealLayout fabRevealLayout = (FABRevealLayout) view.findViewById(R.id.fab_reveal_layout);
+        configureFABReveal(fabRevealLayout);
+        img = (ImageView) view.findViewById(R.id.game_cover_image);
         final Handler handler = new Handler();
+
         Runnable runnable = new Runnable() {
             int i = 0;
 
@@ -50,6 +61,60 @@ public class BeforeWorkoutFragment extends Fragment {
 
         progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
         textView = (TextView) view.findViewById(R.id.textView);
+        fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        pause = (ImageButton) view.findViewById(R.id.imageButtonPause);
+
+        th = new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus += 1;
+                    // Update the progress bar and display the
+                    //current value in the text view
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                            textView.setText(progressStatus+"/"+progressBar.getMax());
+                        }
+                    });
+                    try {
+                        // Sleep for 200 milliseconds.
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
+        });
+       /* fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                new Thread(new Runnable() {
+                    public void run() {
+                        while (progressStatus < 100) {
+                            progressStatus += 1;
+                            // Update the progress bar and display the
+                            //current value in the text view
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    progressBar.setProgress(progressStatus);
+                                    textView.setText(progressStatus+"/"+progressBar.getMax());
+                                }
+                            });
+                            try {
+                                // Sleep for 200 milliseconds.
+                                Thread.sleep(200);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start();
+            }
+        });
+        /*
         // Start long running operation in a background thread
         new Thread(new Runnable() {
             public void run() {
@@ -72,5 +137,60 @@ public class BeforeWorkoutFragment extends Fragment {
                 }
             }
         }).start();
+        */
+    }
+
+    public void launchW(){
+        // Start long running operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus += 1;
+                    // Update the progress bar and display the
+                    //current value in the text view
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                            textView.setText(progressStatus);
+                        }
+                    });
+                    try {
+                        // Sleep for 200 milliseconds.
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+    private void configureFABReveal(FABRevealLayout fabRevealLayout) {
+        fabRevealLayout.setOnRevealChangeListener(new OnRevealChangeListener() {
+            @Override
+            public void onMainViewAppeared(FABRevealLayout fabRevealLayout, View mainView) {}
+
+            @Override
+            public void onSecondaryViewAppeared(final FABRevealLayout fabRevealLayout, View secondaryView) {
+                prepareBackTransition(fabRevealLayout);
+            }
+        });
+    }
+
+    private void prepareBackTransition(final FABRevealLayout fabRevealLayout) {
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                th.interrupt();
+                fabRevealLayout.revealMainView();
+            }
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                th.start();
+                //
+            }
+        }, 2000);
     }
 }
