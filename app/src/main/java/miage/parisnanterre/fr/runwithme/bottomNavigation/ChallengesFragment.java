82369c6cbe47a1b955d5694697634aa199869a6a
Fabien <fabien.michl@gmail.com>
2018-11-12 16:52:47 +0100

    Update : group fragment

--- app/src/main/java/miage/parisnanterre/fr/runwithme/bottomNavigation/ChallengesFragment.java
+++ app/src/main/java/miage/parisnanterre/fr/runwithme/bottomNavigation/ChallengesFragment.java
@@ -2,9 +2,26 @@ package miage.parisnanterre.fr.runwithme.bottomNavigation;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import com.nightonke.boommenu.Animation.BoomEnum;
+import com.nightonke.boommenu.BoomButtons.ButtonPlaceEnum;
+import com.nightonke.boommenu.BoomButtons.HamButton;
+import com.nightonke.boommenu.BoomButtons.SimpleCircleButton;
+import com.nightonke.boommenu.BoomButtons.TextInsideCircleButton;
+import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.ButtonEnum;
+import com.nightonke.boommenu.Piece.PiecePlaceEnum;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import miage.parisnanterre.fr.runwithme.R;
 
@@ -20,9 +37,49 @@ public class ChallengesFragment extends Fragment {
 
     // This event is triggered soon after onCreateView().
     // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    private BoomMenuButton bmb;
+    private ArrayList<Pair> piecesAndButtons = new ArrayList<>();
+    LinearLayout soloLayout;
+    LinearLayout grpLayout;
+    LinearLayout eventLayout;
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Setup any handles to view objects here
         // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+
+        soloLayout = (LinearLayout) view.findViewById(R.id.solo);
+        grpLayout = (LinearLayout) view.findViewById(R.id.grp);
+        eventLayout = (LinearLayout) view.findViewById(R.id.event);
+        final BoomMenuButton bmb = (BoomMenuButton) view.findViewById(R.id.bmb);
+        bmb.setButtonEnum(ButtonEnum.TextInsideCircle);
+        bmb.setPiecePlaceEnum(PiecePlaceEnum.DOT_9_1);
+        bmb.setButtonPlaceEnum(ButtonPlaceEnum.SC_9_1);
+        for (int i = 0; i < bmb.getPiecePlaceEnum().pieceNumber(); i++) {
+            TextInsideCircleButton.Builder builder = new TextInsideCircleButton.Builder()
+                    .normalImageRes(R.drawable.butterfly)
+                    .normalText("Butter Doesn't fly!");
+            bmb.addBuilder(builder);
+        }
+        /*
+        bmb.setPiecePlaceEnum(PiecePlaceEnum.DOT_3_1);
+        bmb.setButtonPlaceEnum(ButtonPlaceEnum.SC_3_3);
+        for (int i = 0; i < bmb.getButtonPlaceEnum().buttonNumber(); i++) {
+            bmb.addBuilder(new SimpleCircleButton.Builder()
+                    .normalImageRes(R.drawable.ic_event));
+        }*/
+
+        grpLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bmb.performClick();
+                bmb.boom();
+            }
+        });
+        eventLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bmb.performClick();
+            }
+        });
     }
 }
