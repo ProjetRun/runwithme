767bb86753553f3cd9ceb2132a37fe086341b66b
Fabien <fabien.michl@gmail.com>
2019-01-13 17:12:13 +0100

    cardGame fix timer bug

--- app/src/main/java/miage/parisnanterre/fr/runwithme/Challenges/VraiFaux.java
+++ app/src/main/java/miage/parisnanterre/fr/runwithme/Challenges/VraiFaux.java
@@ -1,24 +1,14 @@
 package miage.parisnanterre.fr.runwithme.Challenges;
 
-import android.graphics.drawable.Drawable;
 import android.os.CountDownTimer;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.target.SimpleTarget;
-import com.bumptech.glide.request.transition.Transition;
 import com.shashank.sony.fancytoastlib.FancyToast;
-import com.yarolegovich.lovelydialog.LovelyInfoDialog;
 import com.yarolegovich.lovelydialog.LovelyStandardDialog;
 
 import java.util.ArrayList;
@@ -160,12 +150,19 @@ public class VraiFaux extends AppCompatActivity {
     public void click_true(View view) {
         checkresult(true);
         changeItem();
+        reloadTimer();
     }
     public void click_false(View view) {
         checkresult(false);
         changeItem();
+        reloadTimer();
     }
 
+    private void reloadTimer(){
+        pauseTimer();
+        resetTimer();
+        startTimer();
+    }
     private void startTimer() {
         mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
             @Override
