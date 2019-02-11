4bcc5068387c4c93b2cee0c1ae53de6533aa3a41
Nico <nicolas.rouge777@gmail.com>
2019-02-11 16:32:31 +0100

    correction defect NaN + correction defect checked_workout

--- app/src/main/java/miage/parisnanterre/fr/runwithme/MarathonTraining/TrainingChoiceActivity.java
+++ app/src/main/java/miage/parisnanterre/fr/runwithme/MarathonTraining/TrainingChoiceActivity.java
@@ -12,8 +12,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.Objects;
-
 import miage.parisnanterre.fr.runwithme.R;
 
 public class TrainingChoiceActivity extends AppCompatActivity {
@@ -22,9 +20,8 @@ public class TrainingChoiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training_choice);
-     //   Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-     //   setSupportActionBar(toolbar);
-
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         Button button = findViewById(R.id.deb_30min_12);
         button.setOnClickListener(new View.OnClickListener() {
@@ -32,11 +29,7 @@ public class TrainingChoiceActivity extends AppCompatActivity {
             public void onClick(View v) {
                 setDefaults("choiceKey", 1, getApplicationContext());
                 Intent intent = new Intent(TrainingChoiceActivity.this, MarathonTrainingActivity.class);
-                Bundle b = new Bundle();
-                b.putString("Valeur", "1");
-                intent.putExtras(b);
                 startActivity(intent);
-                finish();
             }
         });
 
@@ -49,11 +42,7 @@ public class TrainingChoiceActivity extends AppCompatActivity {
             public void onClick(View v) {
                 setDefaults("choiceKey", 2, getApplicationContext());
                 Intent intent = new Intent(TrainingChoiceActivity.this, MarathonTrainingActivity.class);
-                Bundle b = new Bundle();
-                b.putString("Valeur", "2");
-                intent.putExtras(b);
                 startActivity(intent);
-                finish();
             }
         });
 
